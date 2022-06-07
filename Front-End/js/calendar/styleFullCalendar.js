90c2ef702c9713ce77dd581bc447ad3daa99beb8
Kalebe Rocha <bezerra.kalebe@gmail.com>
2022-06-07 18:14:14 -0300

    Estilizações do fullCalendar

--- Front-End/js/calendar/styleFullCalendar.js
+++ Front-End/js/calendar/styleFullCalendar.js
@@ -1,13 +1,3 @@
-var containerCalendar = document.querySelector("#calendar") //,
-/*   w = window,
-  d = document,
-  e = d.documentElement,
-  g = d.getElementsByTagName("body")[0],
-  x = w.innerWidth || e.clientWidth || g.clientWidth,
-  y = w.innerHeight || e.clientHeight || g.clientHeight;
-
-containerCalendar.style.top = y / 2 - containerCalendar.clientHeight / 2 + "px";
-containerCalendar.style.left = x / 2 - containerCalendar.clientWidth / 2 + "px";
-
-console.log(containerCalendar + "  ESTÁ SENDO RECEBIDA A STYLE")
- */
+// Estilizando o evento dentro do fullCalendar
+$('.fc-event').css('font-size','1em');
+$('.fc-event').css('width','')
